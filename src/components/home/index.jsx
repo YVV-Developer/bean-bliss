1ac158ca0f43cdb1d147a6230a8f3f0dafa1fa9a
VatsalSutariyaDeveloper <developervatsalsutariya@gmail.com>
2023-12-12 14:00:13 +0530

    change

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import light from '../../assets/images/light.png';
+import headerLine from '../../assets/images/header-line.png';
+import tableSet from '../../assets/images/table-set.png';
 
 const Hero = () => {
-    return (
-        <div>
-            Hero
+  return (
+    <div>
+      <section className="tm-welcome-section">
+        <div className="container tm-position-relative">
+          <div className="tm-lights-container">
+            <img src={light} alt="Light" className="light light-1" />
+            <img src={light} alt="Light" className="light light-2" />
+            <img src={light} alt="Light" className="light light-3" />
+          </div>
+          <div className="row tm-welcome-content">
+            <h2 className="white-text tm-handwriting-font tm-welcome-header">
+              <img src={headerLine} alt="Line" className="tm-header-line" />&nbsp;Welcome To&nbsp;&nbsp;
+              <img src={headerLine} alt="Line" className="tm-header-line" />
+            </h2>
+            <h2 className="gold-text tm-welcome-header-2">Cafe House</h2>
+            <p className="gray-text tm-welcome-description">
+              Cafe House template is a mobile-friendly responsive <span className="gold-text">Bootstrap v3.3.5</span> layout by <span className="gold-text">templatemo</span>. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculusnec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            </p>
+            <a href="#main" className="tm-more-button tm-more-button-welcome">Details</a>
+          </div>
+          <img src={tableSet} alt="Table Set" className="tm-table-set img-responsive" />
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
