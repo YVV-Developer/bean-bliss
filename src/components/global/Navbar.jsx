c340040ed223ea2a12b582fccbae8cfb9527bd63
VatsalSutariyaDeveloper <developervatsalsutariya@gmail.com>
2023-12-12 15:23:47 +0530

    'add footer'

--- src/components/global/Navbar.jsx
+++ src/components/global/Navbar.jsx
@@ -3,15 +3,15 @@ import { NavLink } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <div id="topbar" class="d-flex align-items-center fixed-top">
-                <div class="container d-flex justify-content-center justify-content-md-between">
+            <div id="topbar" className="d-flex align-items-center fixed-top">
+                <div className="container d-flex justify-content-center justify-content-md-between">
 
-                    <div class="contact-info d-flex align-items-center">
-                        <i class="bi bi-phone d-flex align-items-center"><span>+1 5589 55488 55</span></i>
-                        <i class="bi bi-clock d-flex align-items-center ms-4"><span> Mon-Sat: 11AM - 23PM</span></i>
+                    <div className="contact-info d-flex align-items-center">
+                        <i className="bi bi-phone d-flex align-items-center"><span>+1 5589 55488 55</span></i>
+                        <i className="bi bi-clock d-flex align-items-center ms-4"><span> Mon-Sat: 11AM - 23PM</span></i>
                     </div>
 
-                    <div class="languages d-none d-md-flex align-items-center">
+                    <div className="languages d-none d-md-flex align-items-center">
                         <ul>
                             <li>En</li>
                             <li><a href="#">De</a></li>
@@ -21,26 +21,26 @@ const Navbar = () => {
             </div>
 
 
-            <header id="header" class="fixed-top d-flex align-items-cente">
-                <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+            <header id="header" className="fixed-top d-flex align-items-cente">
+                <div className="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
 
-                    <h1 class="logo me-auto me-lg-0"><a href="index.html">Restaurantly</a></h1>
+                    <h1 className="logo me-auto me-lg-0"><a href="index.html">Restaurantly</a></h1>
 
 
 
-                    <nav id="navbar" class="navbar order-last order-lg-0">
+                    <nav id="navbar" className="navbar order-last order-lg-0">
                         <ul>
-                            <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
-                            <li><a class="nav-link scrollto" href="#about">About</a></li>
-                            <li><NavLink class="nav-link scrollto" to="/menu">Menu</NavLink></li>
-                            <li><a class="nav-link scrollto" href="#specials">Specials</a></li>
-                            <li><a class="nav-link scrollto" href="#events">Events</a></li>
-                            <li><a class="nav-link scrollto" href="#chefs">Chefs</a></li>
-                            <li><a class="nav-link scrollto" href="#gallery">Gallery</a></li>
-                            <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+                            <li><a className="nav-link scrollto active" href="/">Home</a></li>
+                            <li><a className="nav-link scrollto" href="#about">About</a></li>
+                            <li><NavLink className="nav-link scrollto" to="/menu">Menu</NavLink></li>
+                            <li><a className="nav-link scrollto" href="#specials">Specials</a></li>
+                            <li><a className="nav-link scrollto" href="#events">Events</a></li>
+                            <li><a className="nav-link scrollto" href="#chefs">Chefs</a></li>
+                            <li><a className="nav-link scrollto" href="#gallery">Gallery</a></li>
+                            <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
                                 <ul>
                                     <li><a href="#">Drop Down 1</a></li>
-                                    <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                                    <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
                                         <ul>
                                             <li><a href="#">Deep Drop Down 1</a></li>
                                             <li><a href="#">Deep Drop Down 2</a></li>
@@ -54,11 +54,11 @@ const Navbar = () => {
                                     <li><a href="#">Drop Down 4</a></li>
                                 </ul>
                             </li>
-                            <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                            <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
                         </ul>
-                        <i class="bi bi-list mobile-nav-toggle"></i>
+                        <i className="bi bi-list mobile-nav-toggle"></i>
                     </nav>
-                    <a href="#book-a-table" class="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
+                    <a href="#book-a-table" className="book-a-table-btn scrollto d-none d-lg-flex">Book a table</a>
 
                 </div>
             </header>
--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -11,43 +11,43 @@ const Hero = () => {
 
   return (
     <>
-      <section id="hero" class="d-flex align-items-center">
-        <div class="container position-relative text-center text-lg-start">
-          <div class="row">
-            <div class="col-lg-8">
+      <section id="hero" className="d-flex align-items-center">
+        <div className="container position-relative text-center text-lg-start">
+          <div className="row">
+            <div className="col-lg-8">
               <h1>Welcome to <span>Restaurantly</span></h1>
               <h2>Delivering great food for more than 18 years!</h2>
-              <div class="btns">
-                <a href="#menu" class="btn-menu animated fadeInUp scrollto">Our Menu</a>
-                <a href="#book-a-table" class="btn-book animated fadeInUp scrollto">Book a Table</a>
+              <div className="btns">
+                <a href="#menu" className="btn-menu animated fadeInUp scrollto">Our Menu</a>
+                <a href="#book-a-table" className="btn-book animated fadeInUp scrollto">Book a Table</a>
               </div>
             </div>
-            <div class="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
-              <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" class="glightbox play-btn"></a>
+            <div className="col-lg-4 d-flex align-items-center justify-content-center position-relative" data-aos="zoom-in" data-aos-delay="200">
+              <a href="https://www.youtube.com/watch?v=u6BOC7CDUTQ" className="glightbox play-btn"></a>
             </div>
           </div>
         </div>
       </section>
 
-      <section id="about" class="about">
-        <div class="container" data-aos="fade-up">
+      <section id="about" className="about">
+        <div className="container" data-aos="fade-up">
 
-          <div class="row">
-            <div class="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100">
-              <div class="about-img">
+          <div className="row">
+            <div className="col-lg-6 order-1 order-lg-2" data-aos="zoom-in" data-aos-delay="100">
+              <div className="about-img">
                 <img src={aboutImg} alt="" />
               </div>
             </div>
-            <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
+            <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
               <h3>Voluptatem dignissimos provident quasi corporis voluptates sit assumenda.</h3>
-              <p class="fst-italic">
+              <p className="fst-italic">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
                 magna aliqua.
               </p>
               <ul>
-                <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-                <li><i class="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-                <li><i class="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                <li><i className="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                <li><i className="bi bi-check-circle"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                <li><i className="bi bi-check-circle"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
               </ul>
               <p>
                 Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
